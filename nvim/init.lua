62bb022648a83740019465d9ee3e98735e9bc52e
adampodkdev@gmail.com <adampodkdev@gmail.com>
2022-04-19 21:48:19 +0200

    Dont show virtual text on diagnostics

--- nvim/init.lua
+++ nvim/init.lua
@@ -118,3 +118,9 @@ function! InsertMapForEnter()
     endif
 endfunction
 ]])
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = false,
+    }
+)
